bf7edb95a5d42d39980cc691c3aa6ca86a812a3d
Müller, Paul Ferdinand <paul-ferdinand.mueller@adesso-orange.com>
2024-05-23 15:03:05 +0200

    first partially functioning version of new approach

--- btt_excel_concat.py
+++ btt_excel_concat.py
@@ -32,31 +32,47 @@ def paste_range(start_col, start_row, end_col, end_row, sheet_receiving, copied_
             try:
                 sheet_receiving.cell(row=i, column=j).value = copied_data[count_row][count_col]
             except Exception as e:
-                sheet_receiving.cell(row=i, column=j).value = None
+                #sheet_receiving.cell(row=i, column=j).value = None
+                continue
             count_col += 1
         count_row += 1
 
 
 for path in file_paths:
     wb_tmp = load_workbook(path)
+    try:
+        wb_tmp.remove(wb_tmp["Übersicht"])
+        wb_tmp.remove(wb_tmp["Quercheck Transaktionen"])
+    except Exception as e:
+        print(str(e))
     tab_data = {}
 
-    for sheet in wb_tmp:    # TODO: was ist mit sheets, die voneinander getrennte Tabellen haben?
-        if sheet != 'Übersicht' and sheet != 'das andere halt':
-            tab_data[sheet] = {
-                "start_col": sheet.min_column,
-                "start_row": sheet.min_row,
-                "end_col": sheet.max_column,
-                "end_row": sheet.max_row
-            }
+    for sheet_name in wb_tmp.sheetnames:    # TODO: was ist mit sheets, die voneinander getrennte Tabellen haben?
+        tab_data[sheet_name] = {
+            "start_col": wb_tmp[sheet_name].min_column,
+            "start_row": wb_tmp[sheet_name].min_row,
+            "end_col": wb_tmp[sheet_name].max_column,
+            "end_row": wb_tmp[sheet_name].max_row
+        }
     all_tab_data[path] = tab_data
 
 for file, data in all_tab_data.items():
     wb_tmp = load_workbook(file)
-    for sheet, dim in data.items():
-        sel_range = copy_range(dim["start_col"], dim["start_row"], dim["end_col"], dim["end_col"], sheet)
-        paste_range(wb[sheet].min_column, dim["start_row"] + wb[sheet].max_row, wb[sheet].max_column, # TODO: wie Dimensionen des Templates behandeln?
-                    dim["end_row"] + wb[sheet].max_row, wb[sheet], sel_range)
+    for sheet_name, dim in data.items():
+        if sheet_name == 'BTT':
+            sel_range = copy_range(dim["start_col"], dim["start_row"]+2, dim["end_col"], dim["end_row"], wb_tmp[sheet_name])
+        else:
+            sel_range = copy_range(dim["start_col"], dim["start_row"]+1, dim["end_col"], dim["end_row"],
+                                   wb_tmp[sheet_name])
+        paste_range(wb[sheet_name].min_column, wb[sheet_name].max_row+1, wb[sheet_name].max_column, wb[sheet_name].max_row+dim["end_row"], wb[sheet_name], sel_range) # TODO: die dinger stimmen nicht
 
-# ws.delete_rows(2)     TODO: index rutscht nach!
+
+# TODO:
+#   - paste_range anpassen
+#   - Duplikate behandeln
+#   - Formatierung erweitern
+#   - Drop down ertweitern  -> DataValidation <https://openpyxl.readthedocs.io/en/2.5/validation.html>, <https://stackoverflow.com/questions/51497731/openpyxl-is-it-possible-to-create-a-dropdown-menu-in-an-excel-sheet>
+#   - Formeln überprüfen -> Sheet neu laden?
+
+
 wb.save('output.xlsx')
