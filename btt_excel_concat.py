f2406db2554bbddaa6358529ad025c91ffc1d2ca
Müller, Paul Ferdinand <paul-ferdinand.mueller@adesso-orange.com>
2024-05-23 16:51:44 +0200

    fixed paste dimensions

--- btt_excel_concat.py
+++ btt_excel_concat.py
@@ -14,6 +14,20 @@ file_paths = filedialog.askopenfilenames(filetypes=[("Excel files", "*.xlsx *.xl
 all_tab_data = {}
 
 
+def get_max_row(sheet, start_col, end_col):
+    max_row = 0
+    if start_col == end_col:
+        for row in range(1, sheet.max_row+1):
+            if sheet.cell(row=row, column=start_col).value is not None:
+                max_row = max(max_row, row)
+    else:
+        for col in range(start_col, end_col, 1):
+            for row in range(1, sheet.max_row+1):
+                if sheet.cell(row=row, column=col).value is not None:
+                    max_row = max(max_row, row)
+    return max_row
+
+
 def copy_range(start_col, start_row, end_col, end_row, sheet):
     range_sel = []
     for i in range(start_row, end_row + 1, 1):
@@ -32,28 +46,63 @@ def paste_range(start_col, start_row, end_col, end_row, sheet_receiving, copied_
             try:
                 sheet_receiving.cell(row=i, column=j).value = copied_data[count_row][count_col]
             except Exception as e:
-                #sheet_receiving.cell(row=i, column=j).value = None
+                #sheet_receiving.cell(row=i, column=j).value = None TODO: braucht man das?
                 continue
             count_col += 1
         count_row += 1
 
 
+'''
+    Konsolidierung Worksheets
+        - Übersicht in [A:B] und [E:H]
+        - Quercheck Transaktionen entfällt
+        - BTT, Formulare, Schnittstellen werden im Gesamten konsolidiert
+        - BPML in [A:D] und [F:J]
+        - Datengrundlage adesso zunächst ignoriert, da immer gleich???
+'''
+
 for path in file_paths:
     wb_tmp = load_workbook(path)
     try:
-        wb_tmp.remove(wb_tmp["Übersicht"])
         wb_tmp.remove(wb_tmp["Quercheck Transaktionen"])
     except Exception as e:
         print(str(e))
+
     tab_data = {}
 
-    for sheet_name in wb_tmp.sheetnames:    # TODO: was ist mit sheets, die voneinander getrennte Tabellen haben?
-        tab_data[sheet_name] = {
-            "start_col": wb_tmp[sheet_name].min_column,
-            "start_row": wb_tmp[sheet_name].min_row,
-            "end_col": wb_tmp[sheet_name].max_column,
-            "end_row": wb_tmp[sheet_name].max_row
-        }
+    for sheet_name in wb_tmp.sheetnames:
+        if sheet_name == "Übersicht":
+            tab_data[sheet_name] = [
+                {"start_col": 1, "start_row": 4, "end_col": 2, "end_row": get_max_row(wb_tmp[sheet_name], 1, 2)},
+                # [A:B]
+                {"start_col": 5, "start_row": 2, "end_col": 8, "end_row": get_max_row(wb_tmp[sheet_name], 5, 8)}
+                # [E:H]
+            ]
+        elif sheet_name == "BPML":
+            tab_data[sheet_name] = [
+                {"start_col": 1, "start_row": 2, "end_col": 4, "end_row": get_max_row(wb_tmp[sheet_name], 1, 4)}, # [A:D]
+                {"start_col": 6, "start_row": 2, "end_col": 10, "end_row": get_max_row(wb_tmp[sheet_name], 6, 10)}  # [F:J]
+            ]
+        elif sheet_name == "Datengrundlage adesso":
+            tab_data[sheet_name] = [
+                {"start_col": 1, "start_row": 2, "end_col": 3, "end_row": get_max_row(wb_tmp[sheet_name], 1, 3)},
+                # [A:C]
+                {"start_col": 5, "start_row": 2, "end_col": 5, "end_row": get_max_row(wb_tmp[sheet_name], 5, 5)},
+                # [E]
+                {"start_col": 7, "start_row": 2, "end_col": 7, "end_row": get_max_row(wb_tmp[sheet_name], 7, 7)},
+                # [G]
+                {"start_col": 9, "start_row": 2, "end_col": 9, "end_row": get_max_row(wb_tmp[sheet_name], 9, 9)},
+                # [I]
+                {"start_col": 11, "start_row": 2, "end_col": 11, "end_row": get_max_row(wb_tmp[sheet_name], 11, 11)}
+                # [I]
+            ]
+        else:
+            tab_data[sheet_name] = {
+                "start_col": wb_tmp[sheet_name].min_column,
+                "start_row": wb_tmp[sheet_name].min_row,
+                "end_col": wb_tmp[sheet_name].max_column,
+                "end_row": wb_tmp[sheet_name].max_row
+            }
     all_tab_data[path] = tab_data
 
 for file, data in all_tab_data.items():
@@ -68,6 +117,7 @@ for file, data in all_tab_data.items():
 
 
 # TODO:
+#   - Übersicht doch mit nehmen 😭
 #   - paste_range anpassen
 #   - Duplikate behandeln
 #   - Formatierung erweitern
