efb5eaf1e9f693250c45687c7664510173825b3e
Müller, Paul Ferdinand <paul-ferdinand.mueller@adesso-orange.com>
2024-05-24 12:03:30 +0200

    fixed the paste_ranges; dictionary build for sheet dimensions

--- btt_excel_concat.py
+++ btt_excel_concat.py
@@ -6,8 +6,8 @@ from openpyxl.reader.excel import load_workbook
 root = tkinter.Tk()
 root.withdraw()
 
-#wb = load_workbook('BTT_Template.xlsx')
-wb = load_workbook(filedialog.askopenfilename(filetypes=[("Excel files", '*.xlsx *.xls')]))
+wb = load_workbook('BTT_Template.xlsx')
+#wb = load_workbook(filedialog.askopenfilename(filetypes=[("Excel files", '*.xlsx *.xls')]))
 
 file_paths = filedialog.askopenfilenames(filetypes=[("Excel files", "*.xlsx *.xls")])
 
@@ -52,15 +52,6 @@ def paste_range(start_col, start_row, end_col, end_row, sheet_receiving, copied_
         count_row += 1
 
 
-'''
-    Konsolidierung Worksheets
-        - Übersicht in [A:B] und [E:H]
-        - Quercheck Transaktionen entfällt
-        - BTT, Formulare, Schnittstellen werden im Gesamten konsolidiert
-        - BPML in [A:D] und [F:J]
-        - Datengrundlage adesso zunächst ignoriert, da immer gleich???
-'''
-
 for path in file_paths:
     wb_tmp = load_workbook(path)
     try:
@@ -77,6 +68,10 @@ for path in file_paths:
                 # [A:B]
                 {"start_col": 5, "start_row": 2, "end_col": 8, "end_row": get_max_row(wb_tmp[sheet_name], 5, 8)}
                 # [E:H]
+            ]
+        elif sheet_name == "BTT":
+            tab_data[sheet_name] = [
+                {"start_col": 1, "start_row": 3, "end_col": wb_tmp[sheet_name].max_column, "end_row": wb_tmp[sheet_name].max_row}
             ]
         elif sheet_name == "BPML":
             tab_data[sheet_name] = [
@@ -108,21 +103,22 @@ for path in file_paths:
 for file, data in all_tab_data.items():
     wb_tmp = load_workbook(file)
     for sheet_name, dim in data.items():
-        if sheet_name == 'BTT':
-            sel_range = copy_range(dim["start_col"], dim["start_row"]+2, dim["end_col"], dim["end_row"], wb_tmp[sheet_name])
+        if isinstance(dim, list):
+            for d in dim:
+                sel_range = copy_range(d["start_col"], d["start_row"], d["end_col"], d["end_row"], wb_tmp[sheet_name])
+                paste_range(d["start_col"], get_max_row(wb[sheet_name], d["start_col"], d["end_col"])+1, d["end_col"], wb[sheet_name].max_row+d["end_row"], wb[sheet_name], sel_range)
         else:
-            sel_range = copy_range(dim["start_col"], dim["start_row"]+1, dim["end_col"], dim["end_row"],
-                                   wb_tmp[sheet_name])
-        paste_range(wb[sheet_name].min_column, wb[sheet_name].max_row+1, wb[sheet_name].max_column, wb[sheet_name].max_row+dim["end_row"], wb[sheet_name], sel_range) # TODO: die dinger stimmen nicht
+            sel_range = copy_range(dim["start_col"], dim["start_row"]+1, dim["end_col"], dim["end_row"], wb_tmp[sheet_name])
+            paste_range(wb[sheet_name].min_column, get_max_row(wb[sheet_name], dim["start_col"], dim["end_col"])+1, wb[sheet_name].max_column,
+                        wb[sheet_name].max_row + dim["end_row"], wb[sheet_name], sel_range)
 
 
 # TODO:
-#   - Übersicht doch mit nehmen 😭
-#   - paste_range anpassen
-#   - Duplikate behandeln
+#   - Duplikate behandeln?
 #   - Formatierung erweitern
 #   - Drop down ertweitern  -> DataValidation <https://openpyxl.readthedocs.io/en/2.5/validation.html>, <https://stackoverflow.com/questions/51497731/openpyxl-is-it-possible-to-create-a-dropdown-menu-in-an-excel-sheet>
 #   - Formeln überprüfen -> Sheet neu laden?
+#   - schön machen
 
 
 wb.save('output.xlsx')
