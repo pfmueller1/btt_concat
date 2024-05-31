a86d9d0071624eda06370004de7405bad62f1e2d
Müller, Paul Ferdinand <paul-ferdinand.mueller@adesso-orange.com>
2024-05-31 13:33:46 +0200

    finished duplicate handling

--- btt_excel_concat.py
+++ btt_excel_concat.py
@@ -1,13 +1,39 @@
 import tkinter
 from tkinter import filedialog
+
+import openpyxl
 from openpyxl.formatting.formatting import ConditionalFormattingList
+from openpyxl.packaging.manifest import Override
 from openpyxl.reader.excel import load_workbook
 from openpyxl.styles.differential import DifferentialStyle
 from openpyxl.utils import column_index_from_string, get_column_letter
+from openpyxl.workbook import Workbook
 from openpyxl.worksheet.datavalidation import DataValidation, DataValidationList
 from openpyxl.styles import PatternFill, Color
 from openpyxl.formatting.rule import Rule
 import xxhash
+from openpyxl.worksheet.worksheet import Worksheet
+
+
+class CustomWorksheet(Worksheet):
+    def delete_rows(self, idx, min_col=None, max_col=None):
+        if min_col and max_col:
+            remainder = range(max(get_max_row(self, min_col, max_col), idx), min(idx+1, get_max_row(self, min_col, max_col))+1)
+            max_col += 1
+        else:
+            remainder = range(max(self.max_row, idx), min(idx+1, self.max_row)+1)
+            min_col = self.min_column
+            max_col = self.max_column+1
+        for row in remainder:
+            for col in range(min_col, max_col):
+                if (row, col) in self._cells:
+                    del self._cells[row, col]
+        self._current_row = self.max_row
+        if not self._cells:
+            self._current_row = 0
+
+
+openpyxl.worksheet.worksheet.Worksheet.delete_rows = CustomWorksheet.delete_rows
 
 
 def get_max_row(sheet, start_col, end_col):
@@ -45,6 +71,7 @@ def paste_range(start_col, start_row, end_col, end_row, sheet_receiving, copied_
                 continue
             count_col += 1
         count_row += 1
+    del_dupes(sheet_receiving, columns=range(start_col, end_col + 1))
 
 
 def add_dv(sheet, dv_list):
@@ -92,22 +119,31 @@ def hash_row(row):
 
 
 def del_dupes(sheet, columns=None):
-    seen_hashes = set()
+    seen_hashes = {}
     del_list = []
 
-    for row_idx, row in enumerate(sheet.iter_rows(min_row=3, max_row=sheet.max_row, values_only=True), start=3):
-        if columns:
-            row_values = [row[col - 1] for col in columns]
-        else:
-            row_values = row
+    if columns:
+        max_row = get_max_row(sheet, min(columns), max(columns))
+    else:
+        max_row = sheet.max_row
+
+    for row_idx in range(2, max_row + 1):
+        row_values = []
+        for col in columns:
+            cell = sheet.cell(row=row_idx, column=col)
+            cell_value = cell.value
+            row_values.append(cell_value)
         row_hash = hash_row(row_values)
         if row_hash in seen_hashes:
             del_list.append(row_idx)
         else:
-            seen_hashes.add(row_hash)
+            seen_hashes[row_hash] = row_idx
 
     for row_idx in reversed(del_list):
-        sheet.delete_rows(row_idx)
+        if columns:
+            sheet.delete_rows(row_idx, min_col=min(columns), max_col=max(columns))
+        else:
+            sheet.delete_rows(row_idx)
 
 
 def main():
@@ -129,10 +165,8 @@ def main():
         for sheet_name in wb_tmp.sheetnames:
             if sheet_name == "Übersicht":
                 tab_data[sheet_name] = [
-                    {"start_col": 1, "start_row": 4, "end_col": 2, "end_row": get_max_row(wb_tmp[sheet_name], 1, 2)},
-                    # [A:B]
-                    {"start_col": 5, "start_row": 2, "end_col": 8, "end_row": get_max_row(wb_tmp[sheet_name], 5, 8)}
-                    # [E:H]
+                    {"start_col": 1, "start_row": 4, "end_col": 2, "end_row": get_max_row(wb_tmp[sheet_name], 1, 2)},  # [A:B]
+                    {"start_col": 5, "start_row": 2, "end_col": 8, "end_row": get_max_row(wb_tmp[sheet_name], 5, 8)}  # [E:H]
                 ]
             elif sheet_name == "BTT":
                 tab_data[sheet_name] = [
@@ -141,23 +175,29 @@ def main():
                 ]
             elif sheet_name == "BPML":
                 tab_data[sheet_name] = [
-                    {"start_col": 1, "start_row": 2, "end_col": 4, "end_row": get_max_row(wb_tmp[sheet_name], 1, 4)},
-                    # [A:D]
-                    {"start_col": 6, "start_row": 2, "end_col": 10, "end_row": get_max_row(wb_tmp[sheet_name], 6, 10)}
-                    # [F:J]
+                    {"start_col": 1, "start_row": 2, "end_col": 4, "end_row": get_max_row(wb_tmp[sheet_name], 1, 4)},  # [A:D]
+                    {"start_col": 6, "start_row": 2, "end_col": 10, "end_row": get_max_row(wb_tmp[sheet_name], 6, 10)}  # [F:J]
+                ]
+            elif sheet_name == "Transaktionen":
+                tab_data[sheet_name] = [
+                    {"start_col": 1, "start_row": 2, "end_col": 7, "end_row": get_max_row(wb_tmp[sheet_name], 1, 7)},  # [A:G]
+                ]
+            elif sheet_name == "Formulare":
+                tab_data[sheet_name] = [
+                    {"start_col": 1, "start_row": 2, "end_col": 3, "end_row": get_max_row(wb_tmp[sheet_name], 1, 3)},  # [A:G]
+                ]
+            elif sheet_name == "Schnittstellen":
+                tab_data[sheet_name] = [
+                    {"start_col": 1, "start_row": 2, "end_col": 6, "end_row": get_max_row(wb_tmp[sheet_name], 1, 6)},  # [A:F]
+                    {"start_col": 8, "start_row": 2, "end_col": 10, "end_row": get_max_row(wb_tmp[sheet_name], 8, 10)}  # [F:J]
                 ]
             elif sheet_name == "Datengrundlage adesso":
                 tab_data[sheet_name] = [
-                    {"start_col": 1, "start_row": 2, "end_col": 3, "end_row": get_max_row(wb_tmp[sheet_name], 1, 3)},
-                    # [A:C]
-                    {"start_col": 5, "start_row": 2, "end_col": 5, "end_row": get_max_row(wb_tmp[sheet_name], 5, 5)},
-                    # [E]
-                    {"start_col": 7, "start_row": 2, "end_col": 7, "end_row": get_max_row(wb_tmp[sheet_name], 7, 7)},
-                    # [G]
-                    {"start_col": 9, "start_row": 2, "end_col": 9, "end_row": get_max_row(wb_tmp[sheet_name], 9, 9)},
-                    # [I]
-                    {"start_col": 11, "start_row": 2, "end_col": 11, "end_row": get_max_row(wb_tmp[sheet_name], 11, 11)}
-                    # [K]
+                    {"start_col": 1, "start_row": 2, "end_col": 3, "end_row": get_max_row(wb_tmp[sheet_name], 1, 3)},  # [A:C]
+                    {"start_col": 5, "start_row": 2, "end_col": 5, "end_row": get_max_row(wb_tmp[sheet_name], 5, 5)},  # [E]
+                    {"start_col": 7, "start_row": 2, "end_col": 7, "end_row": get_max_row(wb_tmp[sheet_name], 7, 7)},  # [G]
+                    {"start_col": 9, "start_row": 2, "end_col": 9, "end_row": get_max_row(wb_tmp[sheet_name], 9, 9)},  # [I]
+                    {"start_col": 11, "start_row": 2, "end_col": 11, "end_row": get_max_row(wb_tmp[sheet_name], 11, 11)}  # [K]
                 ]
             else:
                 tab_data[sheet_name] = {
@@ -178,13 +218,18 @@ def main():
                     paste_range(d["start_col"], get_max_row(wb[sheet_name], d["start_col"], d["end_col"]) + 1,
                                 d["end_col"],
                                 wb[sheet_name].max_row + d["end_row"], wb[sheet_name], sel_range)
+                    # TODO - delete dupes here  -> the current ranges are the ranges of the tmp_wb, not the final wb??  -> call this function in paste_..()?
+                    #del_dupes(wb[sheet_name], columns=range(d["start_col"], d["end_col"]+1))
+
             else:
-                sel_range = copy_range(dim["start_col"], dim["start_row"] + 1, dim["end_col"], dim["end_row"],
+                sel_range = copy_range(dim["start_col"] + 1, dim["start_row"] + 1, dim["end_col"], dim["end_row"],  # TODO: start_col + 1??
                                        wb_tmp[sheet_name])
                 paste_range(wb[sheet_name].min_column,
                             get_max_row(wb[sheet_name], dim["start_col"], dim["end_col"]) + 1,
                             wb[sheet_name].max_column,
                             wb[sheet_name].max_row + dim["end_row"], wb[sheet_name], sel_range)
+                # TODO - delete dupes here
+                #del_dupes(wb[sheet_name], columns=range(dim["start_col"], dim["end_col"]+1))
 
             # expand table dimensions in template file
             if sheet_name == "Übersicht":
@@ -200,6 +245,7 @@ def main():
                 wb[sheet_name].tables["Formulare"].ref = f"$A$1:$C${get_max_row(wb[sheet_name], 1, 3)}"
             elif sheet_name == "Schnittstellen":
                 wb[sheet_name].tables["Schnittstelle_Klarname"].ref = f"$H$1:$J${get_max_row(wb[sheet_name], 8, 10)}"
+                wb[sheet_name].tables["Schnittstellen_technisch"].ref = f"$A$1:$F${get_max_row(wb[sheet_name], 1, 6)}"
             elif sheet_name == "Datengrundlage adesso":
                 wb[sheet_name].tables["Module"].ref = f"$A$1:$C${get_max_row(wb[sheet_name], 1, 3)}"
                 wb[sheet_name].tables["Prioritäten"].ref = f"$E$1:$E${get_max_row(wb[sheet_name], 5, 5)}"
@@ -241,10 +287,7 @@ def main():
     #   - there also seems to be a problem with the new file when opening
     #   - column AL? - aktivesTeilprojekt??
 
-
-
-    del_dupes(wb["Übersicht"], columns=[5, 6, 7, 8])    # TODO -> muss angepasst werden, sonst werden die Tabellen schon erweitert
-    del_dupes(wb["Übersicht"], columns=[1, 2])          #   - außerdem schöner machen, nicht alles hardcoden
+    #del_dupes(wb["Datengrundlage adesso"], columns=[1, 2, 3])
 
     # modify data validations
     wb["BTT"].data_validations = DataValidationList()
