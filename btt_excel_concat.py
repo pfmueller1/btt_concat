1e1b7e1e0ba66187975403a041fb439d55077afe
Müller, Paul Ferdinand <paul-ferdinand.mueller@adesso-orange.com>
2024-06-03 14:54:07 +0200

    optimized version works

--- btt_excel_concat.py
+++ btt_excel_concat.py
@@ -1,83 +1,36 @@
 import tkinter
 from tkinter import filedialog
-
-import openpyxl
 from openpyxl.formatting.formatting import ConditionalFormattingList
-from openpyxl.packaging.manifest import Override
 from openpyxl.reader.excel import load_workbook
 from openpyxl.styles.differential import DifferentialStyle
 from openpyxl.utils import column_index_from_string, get_column_letter
-from openpyxl.workbook import Workbook
 from openpyxl.worksheet.datavalidation import DataValidation, DataValidationList
 from openpyxl.styles import PatternFill, Color
 from openpyxl.formatting.rule import Rule
 import xxhash
-from openpyxl.worksheet.worksheet import Worksheet
-
-
-class CustomWorksheet(Worksheet):
-    def delete_rows(self, idx, min_col=None, max_col=None):
-        if min_col and max_col:
-            remainder = range(max(get_max_row(self, min_col, max_col), idx), min(idx+1, get_max_row(self, min_col, max_col))+1)
-            max_col += 1
-        else:
-            remainder = range(max(self.max_row, idx), min(idx+1, self.max_row)+1)
-            min_col = self.min_column
-            max_col = self.max_column+1
-        for row in remainder:
-            for col in range(min_col, max_col):
-                if (row, col) in self._cells:
-                    del self._cells[row, col]
-        self._current_row = self.max_row
-        if not self._cells:
-            self._current_row = 0
-
-
-openpyxl.worksheet.worksheet.Worksheet.delete_rows = CustomWorksheet.delete_rows
 
 
 def get_max_row(sheet, start_col, end_col):
     max_row = 0
-    if start_col == end_col:
+    for col in range(start_col, end_col + 1):
         for row in range(1, sheet.max_row + 1):
-            if sheet.cell(row=row, column=start_col).value is not None:
+            if sheet.cell(row=row, column=col).value is not None:
                 max_row = max(max_row, row)
-    else:
-        for col in range(start_col, end_col, 1):
-            for row in range(1, sheet.max_row + 1):
-                if sheet.cell(row=row, column=col).value is not None:
-                    max_row = max(max_row, row)
     return max_row
 
 
 def copy_range(start_col, start_row, end_col, end_row, sheet):
-    range_sel = []
-    for i in range(start_row, end_row + 1, 1):
-        row_sel = []
-        for j in range(start_col, end_col + 1, 1):
-            row_sel.append(sheet.cell(row=i, column=j).value)
-        range_sel.append(row_sel)
-    return range_sel
+    return [[sheet.cell(row=i, column=j).value for j in range(start_col, end_col + 1)] for i in range(start_row, end_row + 1)]
 
 
-def paste_range(start_col, start_row, end_col, end_row, sheet_receiving, copied_data):
-    count_row = 0
-    for i in range(start_row, end_row + 1, 1):
-        count_col = 0
-        for j in range(start_col, end_col + 1, 1):
-            try:
-                sheet_receiving.cell(row=i, column=j).value = copied_data[count_row][count_col]
-            except Exception:
-                continue
-            count_col += 1
-        count_row += 1
-    del_dupes(sheet_receiving, columns=range(start_col, end_col + 1))
-    print(sheet_receiving)
+def paste_range(start_col, start_row, sheet_receiving, copied_data):
+    for i, row_data in enumerate(copied_data, start=start_row):
+        for j, value in enumerate(row_data, start=start_col):
+            sheet_receiving.cell(row=i, column=j).value = value
 
 
 def add_dv(sheet, dv_list):
     max_row = sheet.max_row
-
     for formula, cols in dv_list.items():
         for col in cols:
             col_letter = col if col.isalpha() else get_column_letter(col)
@@ -101,9 +54,7 @@ def add_cf(sheet, cf_list):
                            bgColor=Color(rgb="FFA7A7",
                                          type="rgb")
                            )
-
     dxf = DifferentialStyle(fill=red_fill)
-
     for formula, cols in cf_list.items():
         for col in cols:
             col_letter = col if col.isalpha() else get_column_letter(col)
@@ -117,34 +68,6 @@ def add_cf(sheet, cf_list):
 def hash_row(row):
     row_str = ''.join([str(cell) for cell in row])
     return xxhash.xxh64(row_str).hexdigest()
-
-
-def del_dupes(sheet, columns=None):
-    seen_hashes = {}
-    del_list = []
-
-    if columns:
-        max_row = get_max_row(sheet, min(columns), max(columns))
-    else:
-        max_row = sheet.max_row
-
-    for row_idx in range(2, max_row + 1):
-        row_values = []
-        for col in columns:
-            cell = sheet.cell(row=row_idx, column=col)
-            cell_value = cell.value
-            row_values.append(cell_value)
-        row_hash = hash_row(row_values)
-        if row_hash in seen_hashes:
-            del_list.append(row_idx)
-        else:
-            seen_hashes[row_hash] = row_idx
-
-    for row_idx in reversed(del_list):
-        if columns:
-            sheet.delete_rows(row_idx, min_col=min(columns), max_col=max(columns))
-        else:
-            sheet.delete_rows(row_idx)
 
 
 def main():
@@ -162,7 +85,6 @@ def main():
             print(str(e))
 
         tab_data = {}
-
         for sheet_name in wb_tmp.sheetnames:
             if sheet_name == "Übersicht":
                 tab_data[sheet_name] = [
@@ -209,23 +131,25 @@ def main():
                 }
         all_tab_data[path] = tab_data
 
+    seen_hashes = {}
     for file, data in all_tab_data.items():
         wb_tmp = load_workbook(file)
         for sheet_name, dim in data.items():
             if isinstance(dim, list):
                 for d in dim:
-                    sel_range = copy_range(d["start_col"], d["start_row"], d["end_col"], d["end_row"],
-                                           wb_tmp[sheet_name])
-                    paste_range(d["start_col"], get_max_row(wb[sheet_name], d["start_col"], d["end_col"]) + 1,
-                                d["end_col"],
-                                wb[sheet_name].max_row + d["end_row"], wb[sheet_name], sel_range)
+                    sel_range = copy_range(d["start_col"], d["start_row"], d["end_col"], d["end_row"], wb_tmp[sheet_name])
+                    for row in sel_range:
+                        row_hash = hash_row(row)
+                        if row_hash not in seen_hashes.get(sheet_name, set()):
+                            seen_hashes.setdefault(sheet_name, set()).add(row_hash)
+                            paste_range(d["start_col"], d["start_row"] + len(seen_hashes[sheet_name]) - 1, wb[sheet_name], [row])
             else:
-                sel_range = copy_range(dim["start_col"] + 1, dim["start_row"] + 1, dim["end_col"], dim["end_row"],
-                                       wb_tmp[sheet_name])
-                paste_range(wb[sheet_name].min_column,
-                            get_max_row(wb[sheet_name], dim["start_col"], dim["end_col"]) + 1,
-                            wb[sheet_name].max_column,
-                            wb[sheet_name].max_row + dim["end_row"], wb[sheet_name], sel_range)
+                sel_range = copy_range(dim["start_col"], dim["start_row"], dim["end_col"], dim["end_row"], wb_tmp[sheet_name])
+                for row in sel_range:
+                    row_hash = hash_row(row)
+                    if row_hash not in seen_hashes.get(sheet_name, set()):
+                        seen_hashes.setdefault(sheet_name, set()).add(row_hash)
+                        paste_range(dim["start_col"], dim["start_row"] + len(seen_hashes[sheet_name]) - 1, wb[sheet_name], [row])
 
             # expand table dimensions in template file
             if sheet_name == "Übersicht":
@@ -284,7 +208,6 @@ def main():
     #   - column AL? - aktivesTeilprojekt??
     #   - optimize performance!
 
-    #del_dupes(wb["Datengrundlage adesso"], columns=[1, 2, 3])
 
     # modify data validations
     wb["BTT"].data_validations = DataValidationList()
